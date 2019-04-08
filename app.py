a36dc877c2099b4c7a3ce91b54fd2b14c3c61a22
AllyChen <ally53628@gmail.com>
2019-04-08 23:22:50 +0800

    Update

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text='我重複一次你說的話: '+event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
