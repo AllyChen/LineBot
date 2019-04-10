b0a0423dd969a0e5a75921fddf05e4ea77351992
AllyChen <ally53628@gmail.com>
2019-04-10 21:22:16 +0800

    Modify

--- app.py
+++ app.py
@@ -53,7 +53,10 @@ def callback():
 def handle_message(event):
     reply = False
     if(event.message.text == 'pottermore'):
-        message = TextSendMessage(text=pottermore())
+        linksting=''
+        for link in pottermore():
+            linksting = linksting + '/n' + link
+        message = TextSendMessage(text=linksting)
         reply = True
     if('艾莉2號' in event.message.text):
         message = TextSendMessage(text=event.message.text.replace("艾莉2號", ""))
