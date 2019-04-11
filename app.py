1a0c93d0d78656f2891f700e5731d6cadedaa67c
AllyChen <ally53628@gmail.com>
2019-04-11 13:04:33 +0800

    Modify

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def callback():
 def handle_message(event):
 
     if(event.message.text == 'pottermore'):
-        texts = pottermore()
+        texts = [1,2,3,4,5]#pottermore()
         messages = []
         for t in texts:
             messages.append(TextSendMessage(text=t))
